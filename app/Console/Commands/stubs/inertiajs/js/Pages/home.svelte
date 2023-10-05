9fe8a328781f53f6d974334abc74b854279f03ec
Orlov Dmitriy <orlovdmi.igor@gmail.com>
2023-10-05 14:19:32 +0300

    feat(Frontend Start Kit) inertia stubs files

---
+++ app/Console/Commands/stubs/inertiajs/js/Pages/home.svelte
@@ -0,0 +1,7 @@
+<script>
+    let user = 'User'
+</script>
+
+<div>
+    <h1>Hello {user}! This page is created with <a target="_blank" href="https://inertiajs.com">InertiaJS</a> and <a target="_blank" href="https://svelte.dev/">Svelte</a></h1>
+</div>
